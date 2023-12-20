938e799c43097f465d445de4ee59e65b448591d4
Feddy's Thinkpad <cavaliere12.web@gmail.com>
2023-12-20 09:57:15 +0100

    firebase instance fix #5

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,18 +1,36 @@
-import { initializeApp } from "firebase-admin/app";
+import { App, initializeApp } from "firebase-admin/app";
 import { credential } from "firebase-admin";
 import { getMessaging } from "firebase-admin/messaging";
 
-export const sendNotification = async (body: string, token: string): Promise<void> =>{
-// export const sendNotification = async (notification: {body: string, title: string}, token: string): Promise<void> =>{
-    return getMessaging(
-        initializeApp({
+export class FirebaseApp {
+    instance: App;
+    constructor() {
+        this.instance = initializeApp({
             credential: credential.cert(process.env.APP_ROOT+"/credentials/firebase-adminsdk.json")
         })
-    ).send({
-        notification: {
-            title: "Pharmate",
-            body
-        },
-        token
-    }).then()
+    }
+    async send(body: string, token: string): Promise<void> {
+        return getMessaging(this.instance).send({
+            notification: {
+                title: "Pharmate",
+                body
+            },
+            token
+        }).then()
+    }
 }
+
+// export const sendNotification = async (body: string, token: string): Promise<void> =>{
+// // export const sendNotification = async (notification: {body: string, title: string}, token: string): Promise<void> =>{
+//     return getMessaging(
+//         initializeApp({
+//             credential: credential.cert(process.env.APP_ROOT+"/credentials/firebase-adminsdk.json")
+//         })
+//     ).send({
+//         notification: {
+//             title: "Pharmate",
+//             body
+//         },
+//         token
+//     }).then()
+// }
